b09e125cc6bcc89da46a0982257440e88e22847e
Gino Carrasco Mori <gino.carrasco@tranzact.net>
2022-01-12 17:41:24 -0500

    updated index.js to export Counter component

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-import { render } from "solid-js/web";
-import { createSignal } from "solid-js";
-
-function Counter() {
-  const [count, setCount] = createSignal(0);
-  const increment = () => setCount(count() + 1);
-
-  return (
-    <button type="button" onClick={increment}>
-      {count()}
-    </button>
-  );
-}
-
-render(() => <Counter />, document.getElementById("app"));
+export { default as Counter } from './components/Counter'
